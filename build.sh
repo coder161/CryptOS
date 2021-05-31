3e3cb11c8564b27d062a957620fea67703e92b88
coder161 <horst-bierpeter@gmx.de>
2021-05-31 19:31:52 +0200

    Made build.sh a better place

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 set -e
-echo "[build.sh] Compiling 'source/Sector 1/bootloader.asm'"
+echo "[ build.sh ] Compiling Assemblies"
+echo "    [*] Compiling 'source/Sector 1/bootloader.asm'"
 nasm source/Sector\ 1/bootloader.asm -f bin -o build/bootloader.bin
-echo "[build.sh] Compiled"
-echo "[build.sh] Compiling 'source/Sector 2+/extendedbootloader.asm'"
+echo "    [*] Compiling 'source/Sector 2+/extendedbootloader.asm'"
 nasm source/Sector\ 2+/extendedbootloader.asm -f elf64 -o build/extendedbootloader.o
-echo "[build.sh] Compiled"
-echo "[build.sh] Compiling 'source/Sector 2+/bins.asm'"
+echo "    [*] Compiling 'source/Sector 2+/bins.asm'"
 nasm source/Sector\ 2+/bins.asm -f elf64 -o build/bins.o
-echo "[build.sh] Compiled"
 
-echo "[build.sh] Compiling 'source/kernel.cpp'"
+echo "[ build.sh ] Compiling C++ Code"
+echo "    [*] Compiling 'source/kernel.cpp'"
 compiler/bin/x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "source/kernel.cpp" -o "build/kernel.o"
-echo "[build.sh] Compiled"
+echo "    [*] Compiling 'source/lib/Heap.cpp'"
+compiler/bin/x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "source/lib/Heap.cpp" -o "build/Heap.o"
 
-echo "[build.sh] Linking"
+
+echo "[ build.sh ] Do some other stuff"
+echo "    [*] Linking"
 compiler/bin/x86_64-elf-ld -T"link.ld"
-echo "[build.sh] Linked"
 
-echo "[build.sh] Creating 'CryptOS.img'"
+echo "    [*] Creating 'CryptOS.img'"
 cat build/bootloader.bin build/kernel.bin > CryptOS.img
+
 echo "[build.sh] Building Finished"
 
 echo "[build.sh] Starting 'run.sh'"
